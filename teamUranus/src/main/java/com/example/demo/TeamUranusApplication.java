68416ac2485ada2091bc1ac8e22ac099398b8705
nerd <nerd@AOI-no-PC>
2023-10-05 21:47:39 +0900

    新規開発

---
+++ teamUranus/src/main/java/com/example/demo/TeamUranusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamUranusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamUranusApplication.class, args);
+	}
+
+}
